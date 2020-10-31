ffe48348948c559ef6089540591ffe2962d135fe
MarcoBroto <msoto9940@gmail.com>
2020-10-31 03:44:36 -0600

    Server and Client can now handle GET and PUT requests
    + Added request buffering to server
    + Added response buffering to client
    + Used base64 encoding to handle more file types

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -1,37 +1,54 @@
 #! python3.8
 
-import os, sys, socket, json
+import os.path as path, sys, socket, json, base64
 
 SERVER_HOST, SERVER_PORT = 'localhost', 9999
 request = {}
 
-def putFile(fpath, socket):
-	with open(fpath, 'br') as file:
-		request['method'] = 'PUT'
-		request['resource'] = fpath
-		request['body'] = file.read().decode()
+def createRequest(method, fpath):
+	request['method'] = method
+	request['resource'] = path.basename(fpath)
+	if method == 'PUT':
+		with open(fpath, 'br') as file:
+			try: request['data'] = base64.b64encode(file.read()).decode('ascii')
+			except Exception as err: print(err)
+	elif method == 'GET':
+		request['method'] = 'GET'
+	else: raise Exception('Request method not supported.')
 
-def getFile(fpath, socket):
-	request['method'] = 'GET'
-	request['resource'] = fpath
 
 if __name__ == "__main__":
 	try:
-		if len(sys.argv) < 2:
-			raise Exception('Expected at least 2 parameters.')
-			sys.exit(1)
+		if len(sys.argv) < 2: raise Exception('Expected at least 2 parameters.')
 		elif len(sys.argv) >= 5: SERVER_HOST, SERVER_PORT = str(sys.argv[3]), sys.argv[4] # Host and port specified
-		fpath = str(sys.argv[1]) # File to send to PUT to server
-		method = str(sys.argv[2]).upper() # Request method
+		method = str(sys.argv[1]).upper()  # Request method
+		fpath = str(sys.argv[2]) # File to send to PUT to server
 
 		sock = socket.create_connection((SERVER_HOST, SERVER_PORT))
-		if method == 'PUT': putFile(fpath, sock) # Send PUT request
-		elif method == 'GET': getFile(fpath, sock) # Send GET request
-		else: raise Exception('Request method not supported.')
+		createRequest(method, fpath) # Structure request
 
-		sock.send(json.dumps(request).encode())
-		response = sock.recv(4096).decode()
-		print(response)
+		# print(request)
+		# r = json.dumps(request).encode()
+		# print(r)
+
+		# Send request to server
+		print('Sending request...')
+		request = json.dumps(request) + '\0' # Add line terminator (null ascii character)
+		sock.send(request.encode())
+
+		print('Receiving response...')
+		blocks = []
+		while buffer := sock.recv(4096).decode(): # Read response
+			if buffer[-1] == '\0': # End of response
+				blocks.append(buffer[:-1])
+				break
+			else: blocks.append(buffer)
+		response = json.loads(''.join(blocks))
+		print(f'{response=}')
+
+		if method == 'GET': # Create file from GET request data
+			with open(response['resource'], 'w') as file: file.write(response['data'])
+
 		sock.shutdown(socket.SHUT_RDWR)
 		sock.close()
 	except Exception as err: print(f'Error: {err}')
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -1,55 +1,79 @@
 #! python3.8
 
-import os, sys, socket, json
+import os, sys, socket, json, base64
 
 HOST, PORT = 'localhost', 9999 # Default host and port
 response = { 'status': 400 } # Default response
+sock = socket.socket() # Server socket object
 
-
-def putFile(request, socket):
+def putFile(fname, data):
 	global response
-	if os.path.exists(request['resource']): # Check if file already exists
-		print('resource found, aborting PUT')
-		response = {'status': 202, 'body': 'File already exists. PUT aborted.'}
-	else: # File does not exists, PUT is executed
-		with open(request['resource'], 'w') as file: file.write(request['body']) # Write file to response body
-		response = {'status': 201, 'body': 'File PUT successfully.'}
+	print(f'Putting file: {fname}')
+	if os.path.exists(fname): # Check if file already exists, skip PUT if true
+		print('resource found, PUT aborted')
+		response = {'status': 409, 'data': 'A file with this name already exists. PUT aborted.'}
+	else: # File does not exist, PUT is executed
+		try:
+			with open(fname, 'bw') as file: file.write(base64.decodebytes(data.encode())) # Write file to response body
+			response = {'status': 201, 'data': 'File PUT successfully'}
+		except Exception as err:
+			print(err)
+			os.remove(fname)  # Delete created file
 
 
-def getFile(request, socket_conn):
-	global response
-	if os.path.exists(request['resource']): # Check if file exists, send file contents
+def getFile(fname):
+	print(f'Getting file: {fname}')
+	if os.path.exists(fname): # Check if file exists, send file contents
 		print('resource found, getting file')
-		with open(request['resource'], 'r') as file: response = {'status': 200, 'body': file.read()}
-	else: response = {'status': 404} # File does not exist
+		with open(fname, 'br') as file:
+			response['status'] = 200
+			response['data'] = base64.b64encode(file.read()).decode('ascii')
+	else: response['status'] = 404 # File does not exist
 
 
-handlers = {'GET': getFile, 'PUT': putFile}
+def handleRequest(req):
+	if req['method'] == 'GET': getFile(request['resource'])
+	elif req['method'] == 'PUT': putFile(request['resource'], request['data'])
 
 
 if __name__ == "__main__":
 	try:
-		if len(sys.argv) >= 3: HOST, PORT = sys.argv[1], sys.argv[2] # Change host and ports with user params
+		if len(sys.argv) == 3: HOST, PORT = sys.argv[1], sys.argv[2] # Change host and ports with user params
+		else:
+			try: HOST = sys.argv[sys.argv.index('-h')+1]
+			except Exception: pass
+			try: PORT = sys.argv[sys.argv.index('-p')+1]
+			except Exception: pass
 
 		# Setup server
-		if not os.path.exists('files'): os.mkdir('./files') # Make file storage directory if it does not exist
-		os.chdir('./files') # Change to file storage directory
-		sock = socket.socket()
+		if not os.path.exists('db'): os.mkdir('./db') # Make file storage directory if it does not exist
+		os.chdir('./db') # Change to file storage directory
 		sock.bind((HOST, PORT))
 
 		sock.listen(10) # Support at most ten connections
 		print(f'Listening on port {PORT}...')
 		while True: # Listen for connections
 			conn, addy = sock.accept()
-			print(f'Connected to {addy}')
+			print(f'\nConnected to {addy}')
 
 			if not os.fork(): # Child process
-				request = json.loads(conn.recv(4096).decode()) # Parse client request
-				print(request)
-				handlers[request['method']](request, sock) # Call mapped request handler
-				print(response)
-				conn.send(json.dumps(response).encode()) # Send response to client
+				blocks = []
+				while buffer := conn.recv(4096).decode(): # Read request
+					if buffer[-1] == '\0':
+						blocks.append(buffer[:-1])
+						break
+					else: blocks.append(buffer)
+				request = json.loads(''.join(blocks))
+
+				handleRequest(request) # Structure response
+				print('Sending Response...')
+				response = json.dumps(response) + '\0' # Add line terminator (null ascii character)
+				conn.send(response.encode()) # Send response to client
 				conn.close() # Close socket connection
+				print('Connection Terminated')
 				sys.exit() # Exit after request fulfilled
-	except Exception as err: print(f'Error: {err}')
+	except Exception as err: print(f'Error: {err}\n')
 	except KeyboardInterrupt: print('Server shutting down...')
+	finally:
+		sock.close()
+		sys.exit(1)
